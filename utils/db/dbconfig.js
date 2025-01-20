dcdc54b22ba63bf5ddf2f9937d12c7459f8b34b0
TejasBhovad <tejasbhovad@gmail.com>
2025-01-20 19:27:02 +0530

    fix: smthn

--- utils/db/dbconfig.js
+++ utils/db/dbconfig.js
@@ -2,7 +2,5 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(
-  "postgresql://neondb_owner:tELKfG4zdQ2A@ep-morning-waterfall-a8tco43v.eastus2.azure.neon.tech/neondb?sslmode=require",
-);
+const sql = neon(process.env.DATABASE_URL);
 export const db = drizzle(sql, { schema });
